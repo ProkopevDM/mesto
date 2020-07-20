e46905f66ac1934a0da2403e3623681236250c22
Dmitry Prokopev <trololbambo@gmail.com>
2020-07-20 20:20:17 +0500

    №1

---
+++ scripts/script.js
@@ -0,0 +1,26 @@
+let popup = document.querySelector('.popup');
+let profile = document.querySelector('.profile');
+let editButton = profile.querySelector('.profile__button-edit');
+let closeButton = popup.querySelector('.popup__button-close');
+let name = profile.querySelector('.profile__name');
+let profession = profile.querySelector('.profile__profession');
+let inputName = popup.querySelector('.popup__input_name');
+let inputProfession = popup.querySelector('.popup__input_profession');
+let formElement = popup.querySelector('.popup__form');
+
+function editProfile() {
+	popup.classList.toggle('popup_opened');
+	inputName.value = name.textContent;
+	inputProfession.value = profession.textContent;
+}
+
+function formSubmitHandler (evt) {
+	evt.preventDefault();
+	name.textContent = inputName.value;
+	profession.textContent = inputProfession.value;
+	editProfile();
+}
+
+editButton.addEventListener('click', editProfile);
+formElement.addEventListener('submit', formSubmitHandler);
+closeButton.addEventListener('click', editProfile);
